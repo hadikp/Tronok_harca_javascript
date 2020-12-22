8735b31e796ff6950362088fe411c732dc03a506
hadikp <hp20001025@gmail.com>
2020-12-22 20:52:46 +0100

    nagykepet kirakom

--- js/got.js
+++ js/got.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let lastName;
-let elem_dead_lastName;
+let first_lastName;
 /* const strArr = [{"str": "The quick brown fox jumps over the lazy dog."}];
 console.log(strArr);
 const words = strArr[0]["str"].split(' ');
@@ -15,17 +15,22 @@ function charData(response) {
     const template = '<div class="portrait"><img class="portrait_img"><figcaption class="figcaption"></figcaption></div>';
     //A halottakat kiszedem a tömbből
     const elem_dead = response.filter(item => item.dead != true);
-    
 
     //Az adatokhoz berakom a lastName-t
      elem_dead.map(item => {
         const name_split = item.name.split(' '); //name-t szétszplitteltem  elem_dead.map(item =>item.name.split(' '));
         if (name_split[1] === undefined) {
              lastName = name_split[0];
-        } else lastName = name_split[1];
+             first_lastName = `${name_split[0]}`; //a name class elkészítése kernev
+        } else {
+            lastName = name_split[1];
+            first_lastName = `${name_split[0]}_${name_split[1]}`; //a name class elkészítése kernev_veznev
+        }
         item.lastName = lastName;
+        return first_lastName; //Ezek kellenek?
+        
      });
-    
+     console.log(first_lastName)
      //Névsorba rendezem
      elem_dead.sort((a, b) => {
          let textA = a.lastName;
@@ -33,13 +38,6 @@ function charData(response) {
          return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
      })
      console.log(elem_dead);
-     console.log(elem_dead_lastName);
-    /* elem_dead.forEach((element, index) => {
-        element.sort((a, b) => a.element.lastName - b.element.lastName);
-    }) */
-     //sort((a, b) => a.lastName - b.lastName);
-    
-    //console.log(elem_dead);
     
     
     //elemek kirakva a DOM-ba
@@ -49,6 +47,16 @@ function charData(response) {
         document.querySelector('.portrait_img').alt = elem_dead[i].name;
         document.querySelector('.figcaption').textContent = elem_dead[i].name.toLocaleUpperCase();     
      }
+
+     //nagyképet kirakom jobbra az aside_left-be
+     const imgEvent = () => {
+        document.querySelector('img[src="assets/petyr.png"]').addEventListener('click', c); 
+     }
+     
+     function c() {
+         console.log('Katt');
+     }
+     imgEvent();
 }
 //Json file beolvasása
 async function getGot(url, options = {}) {
